04156c9bc6b4764189b0ba5da4a33dc3051cc7df
SaiManikanta089 <ksaimanikantak2007@gmail.com>
2026-09-18 16:08:52 +0530

    Time: 11 ms (86.65%), Space: 42.6 MB (42.82%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.c
@@ -0,0 +1,38 @@
+
+
+
+typedef struct {
+    int q[10000];
+    int front;
+    int rear;
+} RecentCounter;
+
+
+RecentCounter* recentCounterCreate() {
+    RecentCounter *obj=malloc(sizeof(RecentCounter));
+    obj->front=0;
+    obj->rear=0;
+    return obj;
+}
+
+int recentCounterPing(RecentCounter* obj, int t) {
+    obj->q[obj->rear]=t;
+    obj->rear++;
+    while(obj->q[obj->front]<t-3000)
+    {
+        obj->front++;
+    }
+    return obj->rear-obj->front;
+}
+
+void recentCounterFree(RecentCounter* obj) {
+    free(obj);
+}
+
+/**
+ * Your RecentCounter struct will be instantiated and called as such:
+ * RecentCounter* obj = recentCounterCreate();
+ * int param_1 = recentCounterPing(obj, t);
+ 
+ * recentCounterFree(obj);
+*/
